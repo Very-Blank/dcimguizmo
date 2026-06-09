b5f4cbbac37140d840e2a2432112ea43752eec9b
very-blank <aapeli.saarelainen.76@gmail.com>
2026-06-09 04:36:16 +0300

    feat: Changes.

--- build.zig
+++ build.zig
@@ -22,28 +22,25 @@ pub fn build(b: *std.Build) void {
     _ = add_files.addCopyFile(dear_bindings.path("src/templates/common-header-template.h"), "common-header-template.h");
     _ = add_files.addCopyFile(dear_bindings.path("src/templates/common-header-template.cpp"), "common-header-template.cpp");
 
-    const sanitize_imguizmo = b.addSystemCommand(&.{
-        "python3",
-    });
-    sanitize_imguizmo.addFileArg(b.path("clean.py"));
+    const clean_command = b.addSystemCommand(&.{"python3"});
+    clean_command.addFileArg(b.path("clean.py"));
+    clean_command.addFileArg(imguizmo_dependency.path("src/ImGuizmo.h"));
 
-    sanitize_imguizmo.addFileArg(imguizmo_dependency.path("src/ImGuizmo.h"));
-    const sanitized_header = sanitize_imguizmo.addOutputFileArg("ImGuizmo.h");
+    const clean_header = clean_command.addOutputFileArg("ImGuizmo.h");
 
     const tmp = b.tmpPath();
-    const run_python = b.addSystemCommand(&.{"python3"});
-    run_python.step.dependOn(&sanitize_imguizmo.step);
-    run_python.setCwd(tmp);
-    run_python.addFileArg(dear_bindings.path("dear_bindings.py"));
-    run_python.addFileArg(sanitized_header);
-    run_python.addArg("-t");
-    run_python.addDirectoryArg(add_files.getDirectory());
-    run_python.addArg("--imconfig-path");
-    run_python.addFileArg(imgui_dependency.path("dcimgui/master/imconfig.h"));
-    run_python.addArg("-o");
-    run_python.addArg("cimguizmo");
+    const dear_command = b.addSystemCommand(&.{"python3"});
+    dear_command.step.dependOn(&clean_command.step);
+    dear_command.setCwd(tmp);
+    dear_command.addFileArg(dear_bindings.path("dear_bindings.py"));
+    dear_command.addFileArg(clean_header);
+    dear_command.addArg("-t");
+    dear_command.addDirectoryArg(add_files.getDirectory());
+    dear_command.addArg("--imconfig-path");
+    dear_command.addFileArg(imgui_dependency.path("dcimgui/master/imconfig.h"));
+    dear_command.addArg("-o");
+    dear_command.addArg("cimguizmo");
 
-    // FIXME: this doesn't wait for the generation to be done
     const cimguizmo_lib = b.addLibrary(.{
         .name = "cimguizmo",
         .root_module = init: {
@@ -64,12 +61,11 @@ pub fn build(b: *std.Build) void {
         .linkage = .static,
     });
 
-    cimguizmo_lib.step.dependOn(&run_python.step);
+    cimguizmo_lib.step.dependOn(&dear_command.step);
 
-    // install
-    // └─ install cimguizmo
-    //    └─ compile lib cimguizmo Debug native 1 errors
-    // error: failed to check cache: '.zig-cache/o/99156590def1e7352807d4d7d9beb15c/cimguizmo/cimguizmo.cpp' file_hash FileNotFound
+    const isntall_file = b.addInstallFile(tmp.path(b, "cimguizmo.h"), "cimguizmo.h");
+    isntall_file.step.dependOn(&dear_command.step);
+    b.getInstallStep().dependOn(&isntall_file.step);
 
     b.installArtifact(cimguizmo_lib);
 }
